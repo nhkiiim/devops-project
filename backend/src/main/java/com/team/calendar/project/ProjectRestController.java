f74d3e1d92a5ec710b7f9b8e610362eab38a8c73
nhkiiim <nhkiiim@naver.com>
2022-11-17 14:02:56 +0900

    feat: add swagger

---
+++ backend/src/main/java/com/team/calendar/project/ProjectRestController.java
@@ -0,0 +1,10 @@
+package com.team.calendar.project;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/project")
+public class ProjectRestController {
+
+}

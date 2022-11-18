83d1c41999fac8574a9638fa52a617706253d190
nhkiiim <nhkiiim@naver.com>
2022-11-18 14:45:56 +0900

    feat: DB 연결 정보 추가 및 JPA 적용

--- backend/src/main/java/com/team/calendar/project/controller/ProjectRestController.java
+++ backend/src/main/java/com/team/calendar/project/controller/ProjectRestController.java
@@ -1,4 +1,4 @@
-package com.team.calendar.project;
+package com.team.calendar.project.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
---
+++ backend/src/main/java/com/team/calendar/project/domain/Project.java
@@ -0,0 +1,13 @@
+package com.team.calendar.project.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+@Entity
+@Getter
+@Setter
+
+public class Project {
+}
